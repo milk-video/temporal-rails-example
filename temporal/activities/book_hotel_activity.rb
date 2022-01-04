96bdf18b3026e8ff5063fc6fc72e34a79c2c9315
antstorm <antstorm@gmail.com>
2022-01-04 10:25:15 +0000

    Remove duplicate activities

--- temporal/activities/book_hotel_activity.rb
+++
@@ -1,9 +0,0 @@
-module Trip
-  class BookHotelActivity < Temporal::Activity
-    def execute(trip_id)
-      logger.info "Booking hotel room", { trip_id: trip_id }
-
-      return { reservation_id: SecureRandom.uuid, total: rand(0..1000) / 10.0 }
-    end
-  end
-end
--- temporal/activities/rent_car_activity.rb
+++
@@ -1,9 +0,0 @@
-module Trip
-  class RentCarActivity < Temporal::Activity
-    def execute(trip_id)
-      logger.info "Renting a car", { trip_id: trip_id }
-
-      return { reservation_id: SecureRandom.uuid, total: rand(0..1000) / 10.0 }
-    end
-  end
-end
